1dc6f47a54da40a5a9c279d7e3c6a075fd093efc
Davide Scrimieri <dscrimie@redhat.com>
2022-05-06 11:33:32 +0200

    .

--- curl-route.sh
+++ curl-route.sh
@@ -1,8 +1,8 @@
 echo getting cookie and storing it in cookies.txt
-curl -s -o /dev/null --cookie-jar cookies.txt http://myspringboot-rai-pod-session-affinity.apps.lab02.gpslab.club/greeting
+curl -s -o /dev/null --cookie-jar cookies.txt http://myspringboot-rai-automated-build-hooks.apps.lab01.gpslab.club/greeting
 
 for i in $(seq 1 $1)
 do
-curl  --cookie cookies.txt  http://myspringboot-rai-pod-session-affinity.apps.lab02.gpslab.club/greeting
+curl  --cookie cookies.txt  http://myspringboot-rai-automated-build-hooks.apps.lab01.gpslab.club/greeting
 printf "\n"
 done 
